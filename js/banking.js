9b206d84c8d45c923769b4be50af35ab102c1733
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-02-11 03:31:24 +0600

    add deposite & balence

---
+++ js/banking.js
@@ -0,0 +1,42 @@
+document.getElementById('deposit-btn').addEventListener('click',function(){
+ 
+    const depositInput = document.getElementById('deposit-input');
+
+    const depositAmountText = depositInput.value;
+    const newDepositAmount = parseFloat(depositAmountText);
+
+    const depositTotal =document.getElementById('deposit-total');
+    const previesDepositAmounttext = depositTotal.innerText;
+    const previesDepositAmount = parseFloat(previesDepositAmounttext);
+    const totalDepositAmount = previesDepositAmount + newDepositAmount;
+    depositTotal.innerText= totalDepositAmount ;
+    // uupdate blance 
+    const totalBalance = document.getElementById('balance-total');
+    const previesBalanceText = totalBalance.innerText;
+    const previesBalance = parseFloat(previesBalanceText);
+    const newtotalBalance = previesBalance + newDepositAmount;
+    totalBalance.innerText = newtotalBalance;
+    //clear deposit input filed value after click
+    depositInput.value = ''; 
+});
+// get Widthdraw
+document.getElementById('widthdraw-btn').addEventListener('click',function(){
+    const widthdrawInput = document.getElementById('widthdraw-input');
+    const newwidthdrawAmountText = widthdrawInput.value;
+    const newwidthdrawAmount = parseFloat(newwidthdrawAmountText);
+
+    const widthdrawTotal = document.getElementById('widthraw-total');
+    const previuswidthdrawAmountText = widthdrawTotal.innerText;
+    const previuswidthdrawAmount = parseFloat(previuswidthdrawAmountText)
+    const totalWidthdraw =  previuswidthdrawAmount + newwidthdrawAmount;
+    widthdrawTotal.innerText = totalWidthdraw;
+    // subtractio after widthdraw
+    const balanceTotal = document.getElementById('balance-total');
+    const prviesBalanceTotalText =  balanceTotal.innerText;
+    const prviesBalanceTotal = parseFloat(prviesBalanceTotalText);
+    const newbalanceTotal = prviesBalanceTotal - newwidthdrawAmount;
+    balanceTotal.innerText = newbalanceTotal;
+    //clear value
+    widthdrawInput.value = '';
+
+})
